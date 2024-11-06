acb6e87c1612447be4bbe649d5c0b49dfded7211
Aryan N. Jain <aryanj1084@gmail.com>
2024-11-06 22:51:46 +0530

    Update Practical no.7.sql

--- Practical No.7/Practical no.7.sql
+++ Practical No.7/Practical no.7.sql
@@ -7,34 +7,34 @@ already exist in the second table then that data should be skipped.
 
 Queries:
 */
-
 Create table oroll(rno int,name varchar(20),contact int);
 Insert into oroll values(1,"qwe",1234),(2,"asd",5678),(3,"zxc",890);
 Create table nroll(rno int,name varchar(20),contact int);
 Insert into nroll values(1,"qwe",1234),(4,"tyu",1357),(5,"hjk",2468);
-Delimiter %
-Create procedure cp1(no int)
-Begin
-Declare exit1 boolean;
-Declare rno1 int;
-Declare name1 varchar(20);
-Declare contactno1 int;
-Declare c1 cursor for select * from oroll where rno=no;
-Declare continue handler for not found set exit1=true;
-Open c1;
-Abc: loop
-Fetch c1 into rno1, name1, contactno1;
-If not exists (select * from nroll where rno=no)then
-Insert into nroll values(rno1,name1,contactno1);
-End if;
-If exit1 then 
-Close c1;
-Leave abc;
-End if;
-End loop abc;
-Select * from oroll;
-End;
-%
-Delimiter ;
-Call cp1(3);
+DELIMITER %
+CREATE PROCEDURE cp1()
+BEGIN
+    DECLARE exit1 BOOLEAN DEFAULT FALSE;
+    DECLARE rno1 INT;
+    DECLARE name1 VARCHAR(20);
+    DECLARE contactno1 INT;
+    DECLARE c1 CURSOR FOR SELECT rno, name, contact FROM nroll;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET exit1 = TRUE;
+    OPEN c1;
+    abc: LOOP
+        FETCH c1 INTO rno1, name1, contactno1;
+        IF exit1 THEN 
+            CLOSE c1;
+            LEAVE abc;
+        END IF;
 
+        IF NOT EXISTS (SELECT * FROM oroll WHERE rno = rno1) THEN
+            INSERT INTO oroll VALUES (rno1, name1, contactno1);
+        END IF;
+    END LOOP abc;
+    SELECT * FROM oroll;
+END %
+
+DELIMITER ;
+
+CALL cp1();
